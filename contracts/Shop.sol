ea51db0a2a4b41c9747cf00700ad3a5c62151342
Shirayuki <32385163+Shira98@users.noreply.github.com>
2020-07-29 23:28:40 +0530

    Update Shop.sol

--- contracts/Shop.sol
+++ contracts/Shop.sol
@@ -3,15 +3,7 @@ pragma solidity >=0.4.21 <0.7.0;
 pragma experimental ABIEncoderV2;
 
 contract Shop{
-
-    address owner;
-
-    //@dev modifer allowing only owner of contract
-    modifier onlyOwner(){
-        require(msg.sender == owner,   "not working");
-        _;
-    }
-
+     
     //Data Structure -
     //@dev creating a struct for wine inventory
     struct WineList{
@@ -31,36 +23,36 @@ contract Shop{
     // }
 
     //Mapping -
-    mapping (uint => WineList) public wines;
-
-    //Variable definitions -
+    WineList[6] public wines;
+    address storeStaff;
     uint public wineCount;
 
-    constructor() public {
-        owner = msg.sender;
+    constructor() public { 
+        
+        storeStaff = msg.sender;
+        
         //@dev List of Wines available in the shop:
-        addWineItem("Duckhorn Merlot", "Napa Valley Three Palms Vineyard",1 wei,1000);
-        addWineItem("K Syrah ", "Walla Walla Valley Powerline Estate",2 wei,1500);
+        addWineItem("Duckhorn Merlot", "Napa Valley Three Palms Vineyard",1,1000);
+        addWineItem("K Syrah ", "Walla Walla Valley Powerline Estate",2,1500);
         addWineItem("Château Coutet", "Barsac",2 wei,2000);
-        addWineItem("Casanova di Neri", "Brunello di Montalcino",1 wei,2000);
+        addWineItem("Casanova di Neri", "Brunello di Montalcino",1,2000);
     }
 
-    function addWineItem(string memory _name, string memory _des, uint _price, uint _qty) private returns (bool){
-        if(wineCount < 10){
+    function addWineItem(string memory _name, string memory _des, uint _price, uint _qty) private { 
             wineCount++;
-            wines[wineCount] = WineList(wineCount, _name, _des, _price, _qty);
-            return true;
-        }
-        else{
-            return false;
-        }
+            wines[wineCount - 1] = WineList(wineCount, _name, _des, _price, _qty);  
+    }
+    
+    function getWineCount() public view returns (uint){
+        return wineCount;
     }
 
-    function getWines(uint _wineIndex) public view returns (string memory, uint ,uint){
-        if(_wineIndex < 10){
-            WineList storage temp = wines[_wineIndex];
-            return (temp.vin_Name, temp.vin_Price, temp.inv_Qty);
+    function getWines(uint _wineIndex) public view   returns (uint ,uint){
+        // require(msg.value <= gasleft());
+        if(_wineIndex <= wineCount - 1){ 
+            return (wines[_wineIndex].vin_Price, wines[_wineIndex].inv_Qty);
         }
+        revert("Wine ID doesn't exist!");
     }
 
     function getDeliveryDate() public pure returns (uint){
@@ -68,7 +60,8 @@ contract Shop{
         return 7;
     }
 
-    function decrementQuantity(uint _wineIndex) private {
-        wines[_wineIndex].inv_Qty--;
+    function decrementWineQuantity(uint _wineIndex, uint _quantityBought) public {
+        wines[_wineIndex].inv_Qty-= _quantityBought;
+        //Delete the item completely if inv_Qty = 0;
     }
 }
