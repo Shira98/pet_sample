7d32e3d353d0fef42dd40ef911747edd1fcaa8a7
Shirayuki <32385163+Shira98@users.noreply.github.com>
2020-08-01 12:55:54 +0530

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,17 +1,11 @@
-var Shop = artifacts.require("./Shop");
- 
+var Store = artifacts.require("./Store");
 var Customer = artifacts.require("./Customer");
-
-// module.exports = function(deployer) {
-//     deployer.deploy(Shop)
-//     .then(()=>{
-//        deployer.deploy(Customer, );
-//    });
-//   };
-
+var Shipping = artifacts.require("./Shipping");
+ 
 module.exports = function(deployer) {
-  deployer.deploy(Shop).then(function(){
-        return deployer.deploy(Customer, Shop.address)
+  deployer.deploy(Store).then(function(){
+        return deployer.deploy(Customer, Store.address)
+}).then(function(){
+    return deployer.deploy(Shipping, Store.address)
 });
 };  
- 
